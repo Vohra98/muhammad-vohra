00029d662aeb6154af8b34f1f2d06be93380e1ab
Muhammad Vohra <muhammad.vohra@oaknorth.co.uk>
2023-12-03 23:01:45 +0000

    fix: testng mobile

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import Header from '@/components/header/Header'
+import Image from 'next/image'
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -20,7 +21,14 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable} font-mont bg-dark text-light`}>
+      <body className={`${montserrat.variable} font-mont bg-dark text-light relative`}>
+        <div className='fixed top-0 left-0 w-screen h-screen -z-10 '>
+          <picture>
+            <source srcSet="/background.png" media="(min-width: 768px)" />
+            <img src="/background-mobile.png" className='w-full h-full object-center object-cover' alt='background'></img>
+          </picture>
+          
+        </div>
         <Header />
         <main>
           {children}
