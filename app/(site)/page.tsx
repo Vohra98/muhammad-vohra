6f697b4f6f72fd1ecd63a21894bd90689f6d34d9
Muhammad Vohra <muhammad.vohra@oaknorth.co.uk>
2023-12-03 23:07:47 +0000

    fix: removing testing content

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -22,36 +22,6 @@ const Home = async () => {
             }
           />
       </div>
-
-      <div className="py-24">
-          <Hero 
-            title={[
-              <span key='1'>Turning your </span>,
-              <span key='2' className="bg-gradient-to-r from-light via-primary to-primaryDark text-transparent bg-clip-text">vision</span> ,
-              <span key='3'> into a reality</span>
-            ]}
-            subtitle="Helping you with your next big project."
-            companies={companies}
-            button={
-              {text: "Get Started", link: "/contact"}
-            }
-          />
-      </div>
-
-      <div className="py-24">
-          <Hero 
-            title={[
-              <span key='1'>Turning your </span>,
-              <span key='2' className="bg-gradient-to-r from-light via-primary to-primaryDark text-transparent bg-clip-text">vision</span> ,
-              <span key='3'> into a reality</span>
-            ]}
-            subtitle="Helping you with your next big project."
-            companies={companies}
-            button={
-              {text: "Get Started", link: "/contact"}
-            }
-          />
-      </div>
     </div>
     
     
