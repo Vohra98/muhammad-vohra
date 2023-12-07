462802ad16e8535455e9eb6c36c2132f53047ae9
Muhammad Vohra <muhammad.vohra@oaknorth.co.uk>
2023-12-07 14:23:08 +0000

    feat: built out homepage projects

--- components/projects/ProjectCard/ProjectCard.tsx
+++ components/projects/ProjectCard/ProjectCard.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { motion, useScroll, useTransform, MotionValue } from "framer-motion";
 import { useState, useEffect, useRef} from "react";
 import Button from "@/components/button/Button";
+import {PortableText} from '@portabletext/react'
 
 const MotionLink = motion(Link);
 
@@ -41,8 +42,10 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
     return (
         <>
             <section className="w-full flex flex-col-reverse lg:flex-row items-center justify-center relative py-24 lg:py-[20vh]" ref={ref} >
-                <motion.div style={{ y }} className="w-1/2 my-8">
-                    <h2 className="text-2xl font-bold pb-16">{project.name}</h2>
+                <motion.div style={{ y }} className="w-1/3 my-8">
+                    <h2 className="text-2xl font-bold pb-8">{project.name}</h2>
+                    {/* <PortableText value={project.content} /> */}
+                    <div className="pb-16">Helping NASA Mission Control personnel navigate and communicate Artemis mission alterations in uncharted lunar regions</div>
                     <Button 
                         text="View Project"
                         url={`/projects/${project.slug}`}
@@ -50,7 +53,7 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
                         className="my-4"
                     />
                 </motion.div>
-                <div className="relative overflow-hidden max-h-[90vh] h-full w-1/2">
+                <div className="relative overflow-hidden max-h-[90vh] h-full w-2/3">
                     <Image 
                     src={project.image}
                     width={900}
